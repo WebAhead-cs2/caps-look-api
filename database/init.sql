78c02e56dde80ff44e085de49839bde4e82a83e0
Saher Hanif <saher.ha97@gmail.com>
2023-01-28 01:46:05 +0200

    updated the tables in the init.sql file

--- database/init.sql
+++ database/init.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-DROP TABLE IF EXISTS employee, role, application, site, country, project, iteration, milestone, scrum CASCADE;
+DROP TABLE IF EXISTS employee, role, application, site, country, project, iteration, milestone, scrum, scrumEmployees CASCADE;
 
 CREATE TABLE employee (
   id SERIAL PRIMARY KEY,
@@ -11,10 +11,8 @@ CREATE TABLE employee (
   productivity VARCHAR (255) NOT NULL,
   site_id INTEGER REFERENCES site(id),
   role_id INTEGER REFERENCES role(id),
-  country_id INTEGER REFERENCES country(id),
   project_id INTEGER REFERENCES project(id),
-  scrum_id INTEGER REFERENCES scrum(id),
-  application_id INTEGER REFERENCES application(id)
+  scrum_id INTEGER REFERENCES scrum(id)
 );
 
 CREATE TABLE role (
@@ -22,15 +20,10 @@ CREATE TABLE role (
    name TEXT
 );
 
-CREATE TABLE application (
-   id SERIAL PRIMARY KEY,
-   name TEXT
-);
-
 CREATE TABLE site (
    id SERIAL PRIMARY KEY,
    country_id INTEGER REFERENCES country(id),
-   location TEXT
+   name TEXT
 );
 
 CREATE TABLE country (
@@ -40,26 +33,41 @@ CREATE TABLE country (
 
 CREATE TABLE project (
    id SERIAL PRIMARY KEY,
-   name TEXT
+   name TEXT,
+   start_date date,
+   end_date date
 );
 
 CREATE TABLE iteration (
    id SERIAL PRIMARY KEY,
    name TEXT,
-   project_id INTEGER REFERENCES project(id)
+   project_id INTEGER REFERENCES project(id),
+   index VARCHAR(255)NOT NULL,
+   start_date date,
+   end_date date
 );
+-- "date" this format YYYY-MM-DD 
 
 CREATE TABLE milestone (
    id SERIAL PRIMARY KEY,
    name TEXT,
-   date TEXT,
-   project_id INTEGER REFERENCES project(id)
+   project_id INTEGER REFERENCES project(id),
+   start_date date,
+   end_date date,
+   description TEXT
 );
 
 CREATE TABLE scrum (
    id SERIAL PRIMARY KEY,
    name TEXT,
-   application_id INTEGER REFERENCES application(id)
+  project_id INTEGER REFERENCES project(id),
+  scrumEmployees_id INTEGER REFERENCES scrumEmployees(id)
+);
+
+CREATE TABLE scrumEmployees (
+   id SERIAL PRIMARY KEY,
+   scrum_master TEXT,
+   employees_id json NOT NULL
 );
 
 commit;
