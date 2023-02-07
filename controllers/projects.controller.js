f712afe7d7328c8afe34b29c319c3233596096f2
AmalZubidat <amalzubidat@gmail.com>
2023-02-07 21:08:04 +0200

    deleted db from controller and response with json instead of send and return message with no projects found in case of data is not valid.

--- controllers/projects.controller.js
+++ controllers/projects.controller.js
@@ -1,13 +1,17 @@
 const catchAsync = require('../utils/catchAsync')
 const ApiError = require('../utils/ApiError')
-const db = require('../database/connection')
 const { getProjects } = require('../database/models/Project')
 const getProjectsController = catchAsync(async (req, res) => {
   const data = await getProjects()
   if (data) {
-    res.status(200).send({
+    res.status(200).json({
       message: 'all projects retrieved successfully.',
       data: data
+    })
+  } else {
+    res.status(200).json({
+      message: 'no projects found',
+      data: ''
     })
   }
 })
