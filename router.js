bc3af2bd76d820f03de090d32566c4cba7ee874a
mahmood odeh <odeh.mahmood3@gmail.com>
2023-02-09 14:24:00 +0200

    added controllers and routes for employee absence and projects

--- router.js
+++ router.js
@@ -1,6 +1,31 @@
 const router = require("express").Router()
 const generalController = require("./controllers/general.controller")
+const absencecontroller = require("./controllers/absence.controller")
+const employeecontroller = require("./controllers/employee.controller")
+const projectscontroller =(require("./controllers/project.controller"))
+
+
 
 router.get("/", generalController.home)
+
+//absence json
+router.get("/getAbsenece",absencecontroller.getAbsenece)
+router.put("/edit-absence",absencecontroller.editabsence)
+router.post("/add-absence", absencecontroller.addabsence)
+router.delete("/delete-absence",absencecontroller.deleteAbsenece)
+
+
+//employee json
+router.get("/getEmployees",employeecontroller.getEmployees)
+router.put("/edit-employee",employeecontroller.editemployee)
+router.post("/add-employee",employeecontroller.addemployee)
+router.delete("/delete-employee",employeecontroller.deleteemployee)
+
+
+// project json
+router.get("/getProjects", projectscontroller.getProjects)
+router.put("/edit-project", projectscontroller.editproject)
+router.post("/add-project",projectscontroller.addproject)
+router.delete("/delete-project",projectscontroller.deleteproject)
 
 module.exports = router
