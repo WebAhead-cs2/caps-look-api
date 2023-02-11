a6e677f6bc8caf2acf2602b6c52139a8ec9caca4
skittan4 <souheilkittan@gmail.com>
2023-02-11 22:46:59 +0200

    enabled all comentout lines

--- router.js
+++ router.js
@@ -11,34 +11,34 @@ router.post('/login', loginController)
 
 router.get(
   '/projects',
-  // verifyToken,
-  // authorizeMiddleware(['scrum_master', 'project_manager', 'resource_manager']),
+   verifyToken,
+   authorizeMiddleware(['scrum_master', 'project_manager', 'resource_manager']),
   projectController.getProjectsController
 )
 router.get(
   '/ProjectPage',
-  // verifyToken,
-  // authorizeMiddleware(['scrum_master', 'project_manager', 'resource_manager']),
+   verifyToken,
+   authorizeMiddleware(['scrum_master', 'project_manager', 'resource_manager']),
   projectController.showProjectsData
 )
 router.post(
   '/AddingProject',
-  // verifyToken,
-  // authorizeMiddleware(['scrum_master', 'project_manager', 'resource_manager']),
+   verifyToken,
+   authorizeMiddleware(['scrum_master', 'project_manager', 'resource_manager']),
   projectController.addingProject
 )
 router.get("/GetPlannedSiteMix/:id",
-  // verifyToken,
-  // authorizeMiddleware(['scrum_master', 'project_manager', 'resource_manager']),
+   verifyToken,
+   authorizeMiddleware(['scrum_master', 'project_manager', 'resource_manager']),
   projectController.getProjectSiteMix)
 
 router.put("/UpdatePlannedSiteMix",
-  // verifyToken,
-  // authorizeMiddleware(['scrum_master', 'project_manager', 'resource_manager']),
+   verifyToken,
+   authorizeMiddleware(['scrum_master', 'project_manager', 'resource_manager']),
   projectController.updateProjectSiteMix)
 
   router.get("/GetActualSiteMix/:id",
-  // verifyToken,
-  // authorizeMiddleware(['scrum_master', 'project_manager', 'resource_manager']),
+   verifyToken,
+   authorizeMiddleware(['scrum_master', 'project_manager', 'resource_manager']),
   projectController.getActualSiteMix)
 module.exports = router
