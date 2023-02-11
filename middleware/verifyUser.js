fca7e22ed031b69b5d7125a6c26f20ce8163ea1d
Saher Hanif <saher.ha97@gmail.com>
2023-02-11 20:44:05 +0200

    fixed db login query

--- middleware/verifyUser.js
+++ middleware/verifyUser.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken')
 require('dotenv').config()
 module.exports = (req, res, next) => {
   const userToken = req.cookies.userToken
+
   if (!userToken) {
     return res.status(403).send({ success: false })
   }
