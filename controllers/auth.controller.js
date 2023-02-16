fbdbf10e79c90e860dcdcdeb2c47f5c1e4d216a1
Astbrqeg <eg.astbrq@gmail.com>
2023-02-16 22:21:54 +0200

    added blacklist node cache db

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,10 @@
+const tokensBlacklist = require('../database/initNodeCache')
+
 const logout = async (req, res) => {
   try {
     res.clearCookie('userToken')
     res.json('logged out')
+    tokensBlacklist.set(req.cookies.userToken, 1)
   } catch {
     res.status(500).json(`failed to logout`)
   }
