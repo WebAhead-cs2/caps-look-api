12cee7a59390e57a4928c0dca4a0e0d3c05ed928
skittan4 <souheilkittan@gmail.com>
2023-02-17 22:33:07 +0200

    fix reviewed items

--- database/connection.js
+++ database/connection.js
@@ -3,7 +3,6 @@ const pg = require('pg')
 require('dotenv').config()
 
 const connectionString = process.env.DATABASE_URL
-console.log('Connection String:' + connectionString)
 const isDev = (process.env.NODE_ENV || 'development') === 'development'
 pg.types.setTypeParser(1082, function (value) {
   return value
